1242a8b6bdfe4370152605da890210991fca1aee
Brian <bdlowery2@gmail.com>
2021-12-24 16:52:06 -0700

    add footer links

--- footer.php
+++ footer.php
@@ -1,8 +1,23 @@
 </main>
 <footer>
-  <div class="inner-column">
-    <h2>footer</h2>
+  <div class="inner-column site-footer">
+    <div class="footer-text">
+      <h3 class="regular-voice"><strong>Brian Lowery</strong></h3>
+      <p>Last updated <time datetime="2021-11-25"> November 25th, 2021</time></p>
+    </div>
+    <div class="footer-links-group">
+      <!-- <nav class="site-menu footer-links">
+        <a href="?page=style-guide" class="">style-guide</a>
+      </nav> -->
+      <nav class="site-menu footer-links">
+        <a href="https://www.linkedin.com/in/bdlowery/">LinkedIn</a>
+        <a href="https://twitter.com/bdlowery2">Twitter</a>
+        <a href="https://codepen.io/bdlowery">CodePen</a>
+        <a href="https://github.com/bdlowery">GitHub</a>
+      </nav>
+    </div>
   </div>
+
 </footer>
 
 <?php wp_footer(); ?>
