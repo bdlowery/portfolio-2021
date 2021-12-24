1a686021beb5a1cdce571dd3128a3d104434ae15
Brian <bdlowery2@gmail.com>
2021-12-24 16:53:45 -0700

    fix sorting of projects by project-type

--- templates/components/project-card.php
+++ templates/components/project-card.php
@@ -1,34 +1,39 @@
 <?php
-$name = get_field("name");
-$projectImage = get_field("project_image");
-$description = get_field("description");
-$toolsUsed = get_field("project_tools");
+//only get the field associated with the correct card ID.
+$name = get_field("name", $card->ID);
+$projectImage = get_field("project_image", $card->ID);
+$description = get_field("description", $card->ID);
+$toolsUsed = get_field("tool", $card->ID);
+$permalink = get_permalink($card->ID);
 
 if ($projectImage) {
   $projectImage = $projectImage["url"];
 } else {
   $projectImage = "https://peprojects.dev/alpha-2/purplebox.jpg";
 }
+
+?>
+<?php
+
 ?>
 
-<a class="project-card-link" href="<?php the_permalink(); ?>">
+<a class="project-card-link" href="<?= $permalink ?>">
   <div class="project-card my-work-project-card">
     <picture class="project-picture" style="background-image: url(<?= $projectImage ?>)">
     </picture>
     <h2 class="stern-voice"><span><?= $name ?></span></h2>
-    <div class="test-flex">
-      <?php if ($toolsUsed) {
-        foreach ($toolsUsed as $tool) {
-          $permalink = get_permalink($tool->ID);
-          $title = get_the_title($tool->ID);
-          $custom_field = get_field("field_name", $tool->ID);
-      ?>
-          <span class="quiet-voice"><?= $title ?></span>
+
+    <?php if ($toolsUsed) { ?>
+      <div class="test-flex">
+        <?php foreach ($toolsUsed as $tool) {
+          echo "hello"; ?>
+
+          <span class="quiet-voice"><?= esc_html($tool->name); ?></span>
         <?php } ?>
-    </div>
+      </div>
 
 
-  <?php } ?>
-  <p class="description-voice"><?= $description ?></p>
+    <?php } ?>
+    <p class="description-voice"><?= $description ?></p>
   </div>
 </a>
