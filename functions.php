ae8d3965e559cce379fc9c625bc836e7b152362e
Brian <bdlowery2@gmail.com>
2022-01-19 16:53:56 -0700

    add login, logo url, title, admin-css, gsap funct.

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-//fix root file path issues with wordpress.
+include("functions/utilities.php");
+
 function getFile($path)
 {
   return dirname(__FILE__) . "/" . $path;
 }
-
 
 // Disable Wordpress Admin Bar for all users
 add_filter("show_admin_bar", "__return_false");
@@ -33,22 +33,35 @@ function register_my_menu()
 }
 add_action("init", "register_my_menu");
 
-// wp_nav_menu( array(
-//   'menu'              => "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
-//   'menu_class'        => "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
-//   'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
-//   'container'         => "", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
-//   'container_class'   => "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
-//   'container_id'      => "", // (string) The ID that is applied to the container.
-//   'fallback_cb'       => "", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
-//   'before'            => "", // (string) Text before the link markup.
-//   'after'             => "", // (string) Text after the link markup.
-//   'link_before'       => "", // (string) Text before the link text.
-//   'link_after'        => "", // (string) Text after the link text.
-//   'echo'              => "", // (bool) Whether to echo the menu or return it. Default true.
-//   'depth'             => "", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
-//   'walker'            => "", // (object) Instance of a custom walker class.
-//   'theme_location'    => "", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
-//   'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
-//   'item_spacing'      => "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
-// ) );
+
+//wp-admin login functions
+
+function my_login_logo_url()
+{
+  return home_url();
+}
+add_filter('login_headerurl', 'my_login_logo_url');
+
+function my_login_logo_url_title()
+{
+  return "Brian Lowery's Portfolio";
+}
+add_filter('login_headertext', 'my_login_logo_url_title');
+
+
+function my_login_stylesheet()
+{
+  wp_enqueue_style('custom-login', get_stylesheet_directory_uri() . '/wp-admin-login.css');
+  wp_enqueue_script('custom-login', get_stylesheet_directory_uri() . '/wp-admin-login.js');
+}
+add_action('login_enqueue_scripts', 'my_login_stylesheet');
+
+
+// The proper way to enqueue GSAP script
+// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+function theme_gsap_script()
+{
+  wp_enqueue_script('gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js', array(), false, true);
+  wp_enqueue_script('gsap-js2', get_template_directory_uri() . '/js/custom-gsap-scripts.js', array(), false, true);
+}
+add_action('wp_enqueue_scripts', 'theme_gsap_script');
