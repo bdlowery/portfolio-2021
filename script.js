b01cebc351e37ecdbcd9717bf52704fb69afb3b4
Brian <bdlowery2@gmail.com>
2022-01-20 20:18:13 -0700

    add more specific selector to js query

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const navLinks = document.querySelectorAll("nav.site-menu > a");
+const navLinks = document.querySelectorAll("div.masthead > nav.site-menu > a");
 
-//add a class site-link to all of the links inside of site-menu.
+//add a class site-link to all of the links inside of the masthead site-menu.
 //NOTE: This is because wordpress sucks.
 navLinks.forEach(function (navLink) {
   navLink.classList.add("site-link");
